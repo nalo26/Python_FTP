081a448fe4a104ebd5021bb32c2e36171e74b93c
Thibault Thomas <thibaulthomas@gmail.com>
2022-09-10 13:15:53 +0200

    port 4021 ('21') for control socket

--- main_client.py
+++ main_client.py
@@ -4,7 +4,7 @@ from client_ftp import ClientFTP
 if __name__ == "__main__":
     client_ftp = ClientFTP()
     client = client_ftp.clientTCP
-    client.connect("127.0.0.1", 4001)
+    client.connect("127.0.0.1", 4021)
 
     while True:
         cmd = input(">>> ")
