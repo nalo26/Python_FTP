824dfbca74a8fe6393fa82753eec607a7eeca406
Thibault Thomas <thibaulthomas@gmail.com>
2022-09-11 11:28:39 +0200

    permissions WIP

--- file_utils.py
+++ file_utils.py
@@ -1,11 +1,22 @@
 import os
 
 
+def is_user_granted_permissions(path: list[str], username: str) -> bool:
+    permissions_file = os.path.join(os.curdir, "/".join(path), "__permissions__")
+    if os.path.exists(permissions_file):
+        with open(permissions_file, "r") as file:
+            lines = file.readlines()
+            if username in lines:
+                return True
+    else:
+        return False
+
+
 def dir(path: list[str]) -> list[str]:
     return os.listdir(os.path.join(os.curdir, "/".join(path)))
 
 
-def cwd(current_path: list[str], new_path: str) -> tuple[list[str], bool]:
+def cwd(current_path: list[str], new_path: str) -> tuple[list[str], bool] or bool:
     updated_path = current_path.copy()
     updated_path.append(new_path)
     if os.path.exists(os.path.join(os.curdir, "/".join(updated_path))):
